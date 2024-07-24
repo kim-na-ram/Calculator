9188ab7c2bc9a187ba2fd6e58738c41c53575c72
kim-na-ram <snrneh3@naver.com>
2024-07-24 11:12:18 +0900

    [FEAT] 무한 반복 기능 추가

--- src/main/java/com/bootcamp/App.java
+++ src/main/java/com/bootcamp/App.java
@@ -8,36 +8,45 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요 : ");
-        int num1 = scanner.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요 : ");
-        int num2 = scanner.nextInt();
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요 : ");
+            int num1 = scanner.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요 : ");
+            int num2 = scanner.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요 : ");
-        char operator = scanner.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요 : ");
+            char operator = scanner.next().charAt(0);
 
-        int result = 0;
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                System.out.println("결과 : " + result);
-                break;
-            case '-':
-                result = num1 - num2;
-                System.out.println("결과 : " + result);
-                break;
-            case '*':
-                result = num1 * num2;
-                System.out.println("결과 : " + result);
-                break;
-            case '/':
-                if(num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            int result;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    System.out.println("결과 : " + result);
                     break;
-                }
-                result = num1 / num2;
-                System.out.println("결과 : " + result);
+                case '-':
+                    result = num1 - num2;
+                    System.out.println("결과 : " + result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    System.out.println("결과 : " + result);
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        break;
+                    }
+                    result = num1 / num2;
+                    System.out.println("결과 : " + result);
+                    break;
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String answer = scanner.next();
+
+            if (answer.equals("exit")) {
                 break;
+            }
         }
     }
 }
