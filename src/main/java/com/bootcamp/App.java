200bc8345781d0c76436c564dd9b5d10c6b9ea98
kim-na-ram <snrneh3@naver.com>
2024-07-24 14:03:31 +0900

    [REFACTOR] 연산 시 Calculate 클래스를 사용하도록 수정

--- src/main/java/com/bootcamp/App.java
+++ src/main/java/com/bootcamp/App.java
@@ -1,6 +1,5 @@
 package com.bootcamp;
 
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -8,7 +7,8 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Queue<Integer> resultQueue = new LinkedList<>();
+        Calculator calculator = new Calculator();
+        Queue<Integer> resultQueue = Calculator.resultQueue;
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요 : ");
@@ -19,47 +19,27 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요 : ");
             char operator = scanner.next().charAt(0);
 
-            int result;
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    resultQueue.offer(result);
-                    System.out.println("결과 : " + result);
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    resultQueue.offer(result);
-                    System.out.println("결과 : " + result);
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    resultQueue.offer(result);
-                    System.out.println("결과 : " + result);
-                    break;
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        break;
-                    }
-                    result = num1 / num2;
-                    resultQueue.offer(result);
-                    System.out.println("결과 : " + result);
-                    break;
+            try {
+                int result = calculator.calculate(num1, num2, operator);
+                System.out.println("결과 : " + result);
+
+                String answer;
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                answer = scanner.next();
+                if (answer.equals("remove")) {
+                    if (!resultQueue.isEmpty()) resultQueue.poll();
+                }
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                answer = scanner.next();
+                if (answer.equals("inquiry")) {
+                    resultQueue.forEach(System.out::println);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
 
             String answer;
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            answer = scanner.next();
-            if (answer.equals("remove")) {
-                if(!resultQueue.isEmpty()) resultQueue.poll();
-            }
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            answer = scanner.next();
-            if (answer.equals("inquiry")) {
-                resultQueue.forEach(System.out::println);
-            }
-
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             answer = scanner.next();
             if (answer.equals("exit")) {
