521c8bf7f7dc5826beae5b24e4801839b5c88d4c
kim-na-ram <snrneh3@naver.com>
2024-07-24 13:34:19 +0900

    [NEW] Calculator, CalculatorExcepion 클래스 추가 [REFACTOR] 연산 수행 및 저장 코드를 Calculator 클래스로 이동 [FEAT] 연산 수행 오류에 대한 예외 처리 추가

---
+++ src/main/java/com/bootcamp/Calculator.java
@@ -0,0 +1,29 @@
+package com.bootcamp;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Calculator {
+    public static Queue<Integer> resultQueue = new LinkedList<>();
+
+    public int calculate(int a, int b, char op) {
+        int result;
+        if (op == '+') {
+            result = a + b;
+        } else if (op == '-') {
+            result = a - b;
+        } else if (op == '*') {
+            result = a * b;
+        } else if (op == '/') {
+            if (b == 0) {
+                throw new CalculatorException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            }
+            result = a / b;
+        } else {
+            throw new CalculatorException(op + "는 잘못된 연산 기호입니다.");
+        }
+
+        resultQueue.add(result);
+        return result;
+    }
+}
