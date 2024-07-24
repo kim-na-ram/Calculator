38145015e7b49baa34c1a3630bbad65c14d8bcea
kim-na-ram <snrneh3@naver.com>
2024-07-24 10:40:55 +0900

    [NEW] 프로젝트 생성
    - JDK 17로 설정
    - `.gitignore`수정
    - App 클래스 생성

---
+++ src/main/java/com/bootcamp/App.java
@@ -0,0 +1,10 @@
+package com.bootcamp;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
