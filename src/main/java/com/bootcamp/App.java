035fc474980b9847b43775bbf55ad5b52d81c323
kim-na-ram <snrneh3@naver.com>
2024-07-24 11:09:08 +0900

    [FEAT] 사칙연산 기능 추가

--- src/main/java/com/bootcamp/App.java
+++ src/main/java/com/bootcamp/App.java
@@ -15,5 +15,29 @@ public class App {
 
         System.out.print("사칙연산 기호를 입력하세요 : ");
         char operator = scanner.next().charAt(0);
+
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("결과 : " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("결과 : " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("결과 : " + result);
+                break;
+            case '/':
+                if(num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = num1 / num2;
+                System.out.println("결과 : " + result);
+                break;
+        }
     }
 }
